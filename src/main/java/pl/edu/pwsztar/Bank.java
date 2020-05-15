75976f57b20a4d9edefe71375cb0db39555e3472
arekop100 <arekop100@gmail.com>
2020-05-16 00:00:21 +0200

    lab1 12

--- src/main/java/pl/edu/pwsztar/Bank.java
+++ src/main/java/pl/edu/pwsztar/Bank.java
@@ -1,37 +1,107 @@
 package pl.edu.pwsztar;
 
-// TODO: Prosze dokonczyc implementacje oraz testy jednostkowe
-// TODO: Prosze nie zmieniac nazw metod - wszystkie inne chwyty dozwolone
-// TODO: (prosze jedynie trzymac sie dokumentacji zawartej w interfejsie BankOperation)
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 class Bank implements BankOperation {
+
+    private final List<Account> accountList = new ArrayList<>();
 
     private static int accountNumber = 0;
 
+    public void addSampleAccounts(List<Account> accounts){
+        accountList.addAll(accounts);
+        accountNumber = accounts.size();
+    }
+
     public int createAccount() {
-        return ++accountNumber;
+        accountList.add(new Account(++accountNumber, 0));
+        return accountNumber;
     }
 
     public int deleteAccount(int accountNumber) {
-        return 0;
+        int balance = ACCOUNT_NOT_EXISTS;
+
+        Account account = findAccount(accountNumber).orElse( new Account(ACCOUNT_NOT_EXISTS,0));
+        if (account.getAccountNumber() != ACCOUNT_NOT_EXISTS) {
+            balance = account.getBalance();
+            accountList.remove(account);
+            Bank.accountNumber--;
+        }
+        return balance;
+
+
     }
 
     public boolean deposit(int accountNumber, int amount) {
+        Account account = findAccount(accountNumber).orElse( new Account(ACCOUNT_NOT_EXISTS,0));
+
+        if(account.getAccountNumber() != ACCOUNT_NOT_EXISTS && amount > 0){
+            account.setBalance(account.getBalance() + amount);
+            return true;
+        }
         return false;
     }
 
     public boolean withdraw(int accountNumber, int amount) {
+        if(amount <= 0){
+            return false;
+        }
+
+        Account account = findAccount(accountNumber).orElse( new Account(ACCOUNT_NOT_EXISTS,0));
+
+        if(account.getAccountNumber() != ACCOUNT_NOT_EXISTS && account.getBalance() >= amount){
+            account.setBalance(account.getBalance() - amount);
+            return true;
+        }
         return false;
     }
 
     public boolean transfer(int fromAccount, int toAccount, int amount) {
+        if(fromAccount == toAccount || amount <= 0){
+            return false;
+        }
+
+        Account fromAcc = findAccount(fromAccount).orElse( new Account(ACCOUNT_NOT_EXISTS,0));
+        Account toAcc = findAccount(toAccount).orElse( new Account(ACCOUNT_NOT_EXISTS,0));
+
+        if(fromAcc.getAccountNumber() != ACCOUNT_NOT_EXISTS && toAcc.getAccountNumber() != ACCOUNT_NOT_EXISTS
+                && fromAcc.getBalance() >= amount){
+
+            fromAcc.setBalance(fromAcc.getBalance() - amount);
+            toAcc.setBalance(toAcc.getBalance() + amount);
+            return true;
+        }
+
         return false;
     }
 
     public int accountBalance(int accountNumber) {
-        return 0;
+        Account account = findAccount(accountNumber).orElse( new Account(ACCOUNT_NOT_EXISTS,0));
+
+        if(account.getAccountNumber() != ACCOUNT_NOT_EXISTS){
+            return account.getBalance();
+        }
+
+        return ACCOUNT_NOT_EXISTS;
     }
 
     public int sumAccountsBalance() {
-        return 0;
+
+        return accountList.stream().mapToInt(Account::getBalance).sum();
     }
+
+    private Optional<Account> findAccount(int accountNumber){
+        for(Account account : accountList){
+            if(account.getAccountNumber() == accountNumber){
+                return Optional.of(account);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+
 }
